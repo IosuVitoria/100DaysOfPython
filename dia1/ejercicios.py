c7a8944af334fda811c28edfd0801753dd26691d
iosu_gomez <iosu.gomez@hodeia.digital>
2025-06-09 22:29:07 +0200

    Dia 1

---
+++ dia1/ejercicios.py
@@ -0,0 +1,19 @@
+# 1. Imprime "Hola, mundo". EXTRA: practica printeando todo lo que se te ocurra.
+
+print("Hola, mundo");
+
+# Extra: Diferentes variables que vamos a imprimir por pantalla.
+
+# Variable de tipo string.
+nombre = "Iosu";
+# Variable del tipo int.
+edad = 39;
+# Variable del tipo float.
+altura = 1.88;
+# Variable del tipo bool.
+estudiante = True;
+
+print(nombre);
+print(edad);
+print(altura);
+print(estudiante);
