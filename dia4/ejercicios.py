9cd752d028b9fb7436dbd2ab957e2da52a1fb2bc
iosu_gomez <iosu.gomez@hodeia.digital>
2025-06-12 21:30:31 +0200

    dia 4

---
+++ dia4/ejercicios.py
@@ -0,0 +1,18 @@
+# 4. Intercambia el valor de dos variables.
+
+# Valores iniciales
+a = 10
+b = 20
+
+# Mostrar antes del intercambio
+print("Antes:")
+print("a =", a)
+print("b =", b)
+
+# Intercambio
+a, b = b, a
+
+# Mostrar después del intercambio
+print("\nDespués:")
+print("a =", a)
+print("b =", b)
