f08f94f8d600ae89765fa18d2554235429c2b960
iosu_gomez <iosu.gomez@hodeia.digital>
2025-06-13 08:29:01 +0200

    Extra

--- dia4/ejercicios.py
+++ dia4/ejercicios.py
@@ -16,3 +16,25 @@ a, b = b, a
 print("\nDespués:")
 print("a =", a)
 print("b =", b)
+
+# Ejercicio extra con 3 variables:
+
+# Valores iniciales
+x = 1
+y = 2
+z = 3
+
+# Mostrar antes del intercambio
+print("Antes:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
+
+# Intercambio
+x, y, z = y, z, x
+
+# Mostrar después
+print("\nDespués:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
