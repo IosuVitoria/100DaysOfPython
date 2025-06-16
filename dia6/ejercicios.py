c3ce6efe74302c27a18834d30f6d8988539dfabb
iosu_gomez <iosu.gomez@hodeia.digital>
2025-06-16 21:07:26 +0200

    dia6

---
+++ dia6/ejercicios.py
@@ -0,0 +1,15 @@
+# 6. Determina si un número es par o impar.
+
+a = 9
+b = 10
+
+def parOimpar(numero):
+    if numero % 2 == 0:
+        return 'par'
+    else:
+        return 'impar'
+    
+print(parOimpar(a));
+print(parOimpar(b));
+
+
